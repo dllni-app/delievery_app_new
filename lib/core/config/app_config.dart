b33ca3b5fe6079bb18db2f3e2f51691a77fde1ec
Mustafa Fares <mustaphafares445@gmail.com>
2026-07-06 19:16:13 +0300

    chore(config): add API base URL config

---
+++ lib/core/config/app_config.dart
@@ -0,0 +1,13 @@
+class AppConfig {
+  AppConfig._();
+
+  static const String apiBaseUrl = String.fromEnvironment(
+    'API_BASE_URL',
+    defaultValue: 'https://alnadha.net/api/v1',
+  );
+
+  static const String driverApiBaseUrl = String.fromEnvironment(
+    'DRIVER_API_BASE_URL',
+    defaultValue: '$apiBaseUrl/delivery/driver',
+  );
+}
