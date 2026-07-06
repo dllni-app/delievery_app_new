da83d2a70f3987dbf224b86b27a3b23d4d7ead9f
Mustafa Fares <mustaphafares445@gmail.com>
2026-07-06 21:27:16 +0300

    export optional driver endpoint support

--- lib/features/driver_app/driver_app.dart
+++ lib/features/driver_app/driver_app.dart
@@ -1,6 +1,8 @@
 export 'data/driver_api_service.dart';
+export 'data/driver_dashboard_models.dart';
 export 'data/driver_data_source_impl.dart';
 export 'data/driver_models.dart';
+export 'data/driver_optional_api_service.dart';
 export 'domain/driver_data_source.dart';
 export 'domain/usecases/driver_usecases.dart';
 export 'presentation/cubit/driver_app_cubit.dart';
