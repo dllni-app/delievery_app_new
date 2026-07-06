516c9d68687f1d12d37a7971b6b0cc9055241f6f
Mustafa Fares <mustaphafares445@gmail.com>
2026-07-06 21:26:59 +0300

    guard driver data source unauthorized responses

--- lib/features/driver_app/data/driver_data_source_impl.dart
+++ lib/features/driver_app/data/driver_data_source_impl.dart
@@ -1,3 +1,8 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../../common/helper/helper.dart';
+import '../../../core/di/injection.dart';
+import '../../../core/unified_api/dio/api_client.dart';
 import '../domain/driver_data_source.dart';
 import 'driver_api_service.dart';
 import 'driver_models.dart';
@@ -9,61 +14,79 @@ class DriverDataSourceImpl implements DriverDataSource {
 
   @override
   Future<LoginResult> login({required String phone, required String password, String? fcmToken}) {
-    return _service.login(phone: phone, password: password, fcmToken: fcmToken);
+    return _guard(() => _service.login(phone: phone, password: password, fcmToken: fcmToken));
   }
 
   @override
-  Future<void> logout() => _service.logout();
+  Future<void> logout() => _guard(_service.logout);
 
   @override
-  Future<DriverModel> getMe() => _service.getMe();
+  Future<DriverModel> getMe() => _guard(_service.getMe);
 
   @override
-  Future<DriverModel> updateAvailability(String availabilityStatus) => _service.updateAvailability(availabilityStatus);
+  Future<DriverModel> updateAvailability(String availabilityStatus) => _guard(() => _service.updateAvailability(availabilityStatus));
 
   @override
   Future<void> postLocation({required double latitude, required double longitude, double? accuracy, double? speed, double? heading}) {
-    return _service.postLocation(latitude: latitude, longitude: longitude, accuracy: accuracy, speed: speed, heading: heading);
+    return _guard(() => _service.postLocation(latitude: latitude, longitude: longitude, accuracy: accuracy, speed: speed, heading: heading));
   }
 
   @override
-  Future<DeliveryAssignmentAttemptModel?> getCurrentOffer() => _service.getCurrentOffer();
+  Future<DeliveryAssignmentAttemptModel?> getCurrentOffer() => _guard(_service.getCurrentOffer);
 
   @override
-  Future<DeliveryOrderModel> acceptOffer(int attemptId) => _service.acceptOffer(attemptId);
+  Future<DeliveryOrderModel> acceptOffer(int attemptId) => _guard(() => _service.acceptOffer(attemptId));
 
   @override
-  Future<void> rejectOffer(int attemptId, String reason) => _service.rejectOffer(attemptId, reason);
+  Future<void> rejectOffer(int attemptId, String reason) => _guard(() => _service.rejectOffer(attemptId, reason));
 
   @override
-  Future<DeliveryOrderModel?> getCurrentOrder() => _service.getCurrentOrder();
+  Future<DeliveryOrderModel?> getCurrentOrder() => _guard(_service.getCurrentOrder);
 
   @override
-  Future<DeliveryOrderModel> startOrder(int orderId) => _service.startOrder(orderId);
+  Future<DeliveryOrderModel> startOrder(int orderId) => _guard(() => _service.startOrder(orderId));
 
   @override
-  Future<DeliveryOrderModel> pickupOrder(int orderId) => _service.pickupOrder(orderId);
+  Future<DeliveryOrderModel> pickupOrder(int orderId) => _guard(() => _service.pickupOrder(orderId));
 
   @override
-  Future<DeliveryOrderModel> deliverOrder(int orderId) => _service.deliverOrder(orderId);
+  Future<DeliveryOrderModel> deliverOrder(int orderId) => _guard(() => _service.deliverOrder(orderId));
 
   @override
-  Future<DeliveryFinancialSummaryModel> getFinancialSummary() => _service.getFinancialSummary();
+  Future<DeliveryFinancialSummaryModel> getFinancialSummary() => _guard(_service.getFinancialSummary);
 
   @override
   Future<List<UserNotificationModel>> getNotifications({int perPage = 20, bool unreadOnly = false}) {
-    return _service.getNotifications(perPage: perPage, unreadOnly: unreadOnly);
+    return _guard(() => _service.getNotifications(perPage: perPage, unreadOnly: unreadOnly));
   }
 
   @override
-  Future<void> markNotificationRead(String id) => _service.markNotificationRead(id);
+  Future<void> markNotificationRead(String id) => _guard(() => _service.markNotificationRead(id));
 
   @override
-  Future<List<DeliveryDisputeModel>> getDisputes({int perPage = 20}) => _service.getDisputes(perPage: perPage);
+  Future<List<DeliveryDisputeModel>> getDisputes({int perPage = 20}) => _guard(() => _service.getDisputes(perPage: perPage));
 
   @override
   String userFacingError(Object error) => _service.userFacingError(error);
 
   @override
   bool isUnauthorized(Object error) => _service.isUnauthorized(error);
+
+  Future<T> _guard<T>(Future<T> Function() request) async {
+    try {
+      return await request();
+    } catch (error) {
+      if (isUnauthorized(error)) {
+        await _clearLocalSession();
+      }
+      rethrow;
+    }
+  }
+
+  Future<void> _clearLocalSession() async {
+    final preferences = getIt<SharedPreferences>();
+    await preferences.remove(PrefsKeys.token);
+    await preferences.remove(PrefsKeys.userInfo);
+    getIt<ApiClient>().resetHeader();
+  }
 }
