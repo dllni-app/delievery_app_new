068e5f2959a8c1449d3ef950fbebd47a6df28b12
Mustafa Fares <mustaphafares445@gmail.com>
2026-07-06 19:28:44 +0300

    update router imports

--- lib/router/app_router.dart
+++ lib/router/app_router.dart
@@ -1,25 +1,9 @@
 import 'package:flutter/material.dart';
 
 import '../features/driver_app/presentation/pages/driver_pages.dart';
+import 'route_names.dart';
 
-class RouteName {
-  RouteName._();
-
-  static const splash = 'splash';
-  static const onBoard = 'onBoard';
-  static const login = 'login';
-  static const signup = 'signup';
-  static const homeNav = 'homeNav';
-  static const confirm = 'confirm';
-  static const forgetPassword = 'forgetPassword';
-  static const forgetPasswordReset = 'forgetPasswordReset';
-  static const notification = 'notification';
-  static const personalInformation = 'personalInformation';
-  static const changePhoto = 'changePhoto';
-  static const search = 'search';
-  static const productDetails = 'productDetails';
-  static const productDetailsTrackScreen = 'productDetailsTrackScreen';
-}
+export 'route_names.dart';
 
 class RouteManager {
   RouteManager._();
@@ -43,12 +27,9 @@ class _RouteNotFoundScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Directionality(
-      textDirection: TextDirection.rtl,
-      child: Scaffold(
-        body: Center(
-          child: Text('الصفحة غير موجودة'),
-        ),
+    return const Scaffold(
+      body: Center(
+        child: Text('Page not found'),
       ),
     );
   }
