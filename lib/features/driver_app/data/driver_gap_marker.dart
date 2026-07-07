e255dad9bba4990ed4185a0b88d4f09e23cafe90
Mustafa Fares <mustaphafares445@gmail.com>
2026-07-07 04:54:41 +0300

    Update marker Arabic escaped test

--- lib/features/driver_app/data/driver_gap_marker.dart
+++ lib/features/driver_app/data/driver_gap_marker.dart
@@ -1,3 +1,3 @@
 // Driver gap implementation marker updated.
 class DriverGapMarker {}
-const bool driverGapImplementationMarker = true;
+const driverGapArabic = '\u0627\u0644\u0645\u0633\u062a\u062d\u0642\u0627\u062a';
