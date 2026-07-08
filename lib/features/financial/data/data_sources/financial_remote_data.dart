cf8e74363fc96e70f55004d37b9dc85f03f67695
Mustafa Fares <mustaphafares445@gmail.com>
2026-07-08 20:43:29 +0300

    Load wallet limits and transactions

--- lib/features/financial/data/data_sources/financial_remote_data.dart
+++ lib/features/financial/data/data_sources/financial_remote_data.dart
@@ -1,9 +1,11 @@
 import 'package:injectable/injectable.dart';
 
+import '../../../../common/helper/src/typedef.dart';
 import '../../../../core/unified_api/api_variables.dart';
 import '../../../../core/unified_api/dio/api_client.dart';
 import '../../../../core/unified_api/error/api_handeler_manager.dart';
 import '../models/financial_summary_response.dart';
+import '../models/financial_transaction_model.dart';
 
 @lazySingleton
 class FinancialRemoteData with HandlingApiManager {
@@ -14,5 +16,18 @@ class FinancialRemoteData with HandlingApiManager {
   Future<FinancialSummaryModel> getFinancialSummary() async => wrapHandlingApi(
         tryCall: () => _apiClient.get(ApiVariables.getFinancialSummary()),
         jsonConvert: financialSummaryModelFromJson,
+      );
+
+  Future<FinancialSummaryModel> getWalletLimits() async => wrapHandlingApi(
+        tryCall: () => _apiClient.get(ApiVariables.getWalletLimits()),
+        jsonConvert: financialSummaryModelFromJson,
+      );
+
+  Future<List<FinancialTransactionModel>> getWalletTransactions(
+    QueryParams params,
+  ) async =>
+      wrapHandlingApi(
+        tryCall: () => _apiClient.get(ApiVariables.getWalletTransactions(params)),
+        jsonConvert: financialTransactionsFromJson,
       );
 }
