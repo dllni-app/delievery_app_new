3501e09755faf82b166644e13b0fd75485370011
Mustafa Fares <mustaphafares445@gmail.com>
2026-07-08 20:43:57 +0300

    Expose wallet financial repository methods

--- lib/features/financial/domain/repositories/financial_repositories.dart
+++ lib/features/financial/domain/repositories/financial_repositories.dart
@@ -1,6 +1,13 @@
 import '../../../../common/helper/src/typedef.dart';
 import '../../data/models/financial_summary_response.dart';
+import '../../data/models/financial_transaction_model.dart';
 
 abstract class FinancialRepositories {
   DataResponse<FinancialSummaryModel> getFinancialSummary();
+
+  DataResponse<FinancialSummaryModel> getWalletLimits();
+
+  DataResponse<List<FinancialTransactionModel>> getWalletTransactions(
+    QueryParams params,
+  );
 }
