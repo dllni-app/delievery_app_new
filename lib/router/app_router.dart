a7838d5a8982f1bccb7d0bc8377533c9c51aca83
AHMADMalbetar <107564019+AHMADMalbetar@users.noreply.github.com>
2026-08-03 18:11:41 +0300

    Route active orders to details

--- lib/router/app_router.dart
+++ lib/router/app_router.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+
 import '../features/auth/presentation/pages/login_screen.dart';
+import '../features/delivery/presentation/pages/delivery_order_details_page.dart';
 import '../features/home/presentation/pages/nav_bar_screen.dart';
 import '../features/notification/presentation/pages/notification_screen.dart';
 import '../features/splash/page/splash_screen.dart';
@@ -8,20 +10,21 @@ import '../features/user/presentation/pages/personal_information_screen.dart';
 class RouteName {
   RouteName._();
 
-  static const splash = "splash";
-  static const onBoard = "onBoard";
-  static const login = "login";
-  static const signup = "signup";
-  static const homeNav = "homeNav";
-  static const confirm = "confirm";
-  static const forgetPassword = "forgetPassword";
-  static const forgetPasswordReset = "forgetPasswordReset";
-  static const notification = "notification";
-  static const personalInformation = "personalInformation";
-  static const changePhoto = "changePhoto";
-  static const search = "search";
-  static const productDetails = "productDetails";
-  static const productDetailsTrackScreen = "productDetailsTrackScreen";
+  static const splash = 'splash';
+  static const onBoard = 'onBoard';
+  static const login = 'login';
+  static const signup = 'signup';
+  static const homeNav = 'homeNav';
+  static const confirm = 'confirm';
+  static const forgetPassword = 'forgetPassword';
+  static const forgetPasswordReset = 'forgetPasswordReset';
+  static const notification = 'notification';
+  static const personalInformation = 'personalInformation';
+  static const deliveryOrderDetails = 'deliveryOrderDetails';
+  static const changePhoto = 'changePhoto';
+  static const search = 'search';
+  static const productDetails = 'productDetails';
+  static const productDetailsTrackScreen = 'productDetailsTrackScreen';
 }
 
 class RouteManager {
@@ -36,7 +39,7 @@ class RouteManager {
         return MaterialPageRoute(builder: (_) => const LoginScreen());
 
       case RouteName.homeNav:
-        return MaterialPageRoute(builder: (_) => NavBarScreen());
+        return MaterialPageRoute(builder: (_) => const NavBarScreen());
 
       case RouteName.notification:
         return MaterialPageRoute(builder: (_) => const NotificationScreen());
@@ -47,8 +50,14 @@ class RouteManager {
           builder: (_) => PersonalInformationScreen(arg: args),
         );
 
+      case RouteName.deliveryOrderDetails:
+        final args = routeSettings.arguments as DeliveryOrderDetailsArgs;
+        return MaterialPageRoute(
+          builder: (_) => DeliveryOrderDetailsPage(initialOrder: args.order),
+        );
+
       default:
-        return MaterialPageRoute(builder: (_) => Container());
+        return MaterialPageRoute(builder: (_) => const SizedBox.shrink());
     }
   }
 }
