73eef3c2e2c0c8597f5b46597f8f9e8f4788e2d6
AHMADMalbetar <107564019+AHMADMalbetar@users.noreply.github.com>
2026-08-11 13:37:00 +0300

    Update base URL to dllni.mustafafares.com

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -3,11 +3,11 @@
 
   static const String apiBaseUrl = String.fromEnvironment(
     'API_BASE_URL',
-    defaultValue: 'https://alnadha.net/api/v1',
+    defaultValue: 'https://dllni.mustafafares.com/api/v1',
   );
 
   static const String driverApiBaseUrl = String.fromEnvironment(
     'DRIVER_API_BASE_URL',
-    defaultValue: 'https://alnadha.net/api/v1/delivery/driver',
+    defaultValue: 'https://dllni.mustafafares.com/api/v1/delivery/driver',
   );
 }
