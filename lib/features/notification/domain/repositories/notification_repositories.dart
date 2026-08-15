21298f607962c834adfdfe80bb978381335106e4
AHMADMalbetar <107564019+AHMADMalbetar@users.noreply.github.com>
2026-08-15 22:20:09 +0300

    Expose delivery notification delete actions

--- lib/features/notification/domain/repositories/notification_repositories.dart
+++ lib/features/notification/domain/repositories/notification_repositories.dart
@@ -1,16 +1,9 @@
 import '../../../../common/helper/src/typedef.dart';
-
 import '../../data/models/get_all_notification_response.dart';
 
-
-
 abstract class NotificationRepositories {
-
   DataResponse<GetAllNotificationResponse> getAllNotification(QueryParams params);
-
-
-
   DataResponse<void> markNotificationRead(String id);
-
+  DataResponse<void> deleteNotification(String id);
+  DataResponse<void> deleteAllNotifications();
 }
-
