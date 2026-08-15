b3a0ff7c7d16b149cf877bc3f1067b50f966fd15
AHMADMalbetar <107564019+AHMADMalbetar@users.noreply.github.com>
2026-08-15 22:20:15 +0300

    Implement delivery notification delete actions

--- lib/features/notification/data/repositories/notification_repositories_imp.dart
+++ lib/features/notification/data/repositories/notification_repositories_imp.dart
@@ -1,60 +1,33 @@
 import 'package:injectable/injectable.dart';
 
 import '../../../../common/helper/src/typedef.dart';
-
 import '../../../../core/unified_api/error/error_handeler.dart';
-
 import '../../domain/repositories/notification_repositories.dart';
-
 import '../data_sources/notification_remote_data.dart';
-
 import '../models/get_all_notification_response.dart';
 
-
-
 @LazySingleton(as: NotificationRepositories)
-
 class NotificationRepositoriesImp
-
     with HandlingException
-
     implements NotificationRepositories {
-
   final NotificationRemoteData _remoteData;
 
-
-
   NotificationRepositoriesImp({required NotificationRemoteData remoteData})
-
       : _remoteData = remoteData;
 
-
+  @override
+  DataResponse<GetAllNotificationResponse> getAllNotification(QueryParams params) async =>
+      wrapHandlingException(tryCall: () => _remoteData.getAllNotification(params));
 
   @override
-
-  DataResponse<GetAllNotificationResponse> getAllNotification(
-
-    QueryParams params,
-
-  ) async =>
-
-      wrapHandlingException(
-
-        tryCall: () => _remoteData.getAllNotification(params),
-
-      );
-
-
-
-  @override
-
   DataResponse<void> markNotificationRead(String id) async =>
+      wrapHandlingException(tryCall: () => _remoteData.markNotificationRead(id));
 
-      wrapHandlingException(
+  @override
+  DataResponse<void> deleteNotification(String id) async =>
+      wrapHandlingException(tryCall: () => _remoteData.deleteNotification(id));
 
-        tryCall: () => _remoteData.markNotificationRead(id),
-
-      );
-
+  @override
+  DataResponse<void> deleteAllNotifications() async =>
+      wrapHandlingException(tryCall: () => _remoteData.deleteAllNotifications());
 }
-
